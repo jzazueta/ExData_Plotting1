0a6cf3e00352f4d82cf5721fa0829b110f2c96ff
Jorge <jorge.zazueta.g@gmail.com>
2014-06-04 13:31:52 -0400

    Eliminated explicit chart limits and added "white" background to chart.

--- Plot1.R
+++ Plot1.R
@@ -17,10 +17,10 @@
 ##      Subsetting data to "2007-02-02" or "2007-02-01"
         power <- power[power$Date=="2007-02-02"| power$Date== "2007-02-01",]
 ##   Creat Plot file    
-        png(file="plot1.png", bg="transparent")
+        png(file="plot1.png", bg="white")
         hist(power$Global_active_power/median(power$Global_active_power), 
              main = paste("Global Active Power"), 
              xlab = "Global Active Power (kilowatts)", 
-             col = "orange red", xlim = c(0,6), ylim = c(0,1200))
+             col = "orange red")
         dev.off()
         
