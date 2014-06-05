1e901c17b3d208db285e609e380f06564d26fdce
Jorge <jorge.zazueta.g@gmail.com>
2014-06-04 22:02:25 -0400

    factor conversion bug fixed

--- Plot1.R
+++ Plot1.R
@@ -6,11 +6,11 @@
 
 ##   Read File
 
-        power <- read.table("household_power_consumption.txt",
-                            sep = ";", header = TRUE)
+        power <- read.csv("household_power_consumption.txt",
+                            sep = ";", header = TRUE, stringsAsFactors = FALSE)
         
 ##   Convert dates, time and numbers
-        power$Time <- strptime(power$Time,"%h:%m:%s")
+        power$Time <- strptime(power$Time,"%H:%M:%S")
         power$Date <- as.Date(power$Date,"%d/%m/%Y")
         power$Global_active_power <- as.numeric(power$Global_active_power)
         
@@ -18,7 +18,7 @@
         power <- power[power$Date=="2007-02-02"| power$Date== "2007-02-01",]
 ##   Creat Plot file    
         png(file="plot1.png", bg="white")
-        hist(power$Global_active_power/median(power$Global_active_power), 
+        hist(power$Global_active_power, 
              main = paste("Global Active Power"), 
              xlab = "Global Active Power (kilowatts)", 
              col = "orange red")
